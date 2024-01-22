c1574ac0752b8098ac259e4daa2f146e8607344d
novakTJ <novak.s.tel@gmail.com>
2024-01-22 20:07:48 +0100

    added reqs file w pip freeze

--- venv/Pisanje.py
+++ venv/Pisanje.py
@@ -1,4 +1,5 @@
-
+import numpy as np
 with open("..\\Bonkball\\Bonkball\\Bonkball\\bin\\Debug\\instructions.txt","w") as f:
-    f.write("-2,2\n")
+    x=np.zeros(3)
+    f.write(f"{x[0]-2,2}\n")
     f.write("1,1\n")
